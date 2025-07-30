51e8c166a624d33f3001c0c9cc779f15274918f3
Shahbaz Khan <carpm.shahbaz@gmail.com>
2025-07-30 15:27:31 +0000

    i will only use this in mobile screen do not strectch screen while open

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,10 +6,14 @@ import { MobileNav } from '@/components/shared/mobile-nav';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppProvider>
-      <main className="flex flex-col h-svh bg-background">
-        {children}
-      </main>
-      <MobileNav />
+      <div className="flex justify-center w-full">
+        <div className="w-full max-w-md bg-background shadow-lg">
+          <main className="flex flex-col h-svh bg-background">
+            {children}
+          </main>
+          <MobileNav />
+        </div>
+      </div>
     </AppProvider>
   );
 }
