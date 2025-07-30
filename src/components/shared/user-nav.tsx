755ccbd6a8b474eb495b765446194d106d10eace
Shahbaz Khan <carpm.shahbaz@gmail.com>
2025-07-30 19:15:54 +0000

    replace with this kind of icon

--- src/components/shared/user-nav.tsx
+++ src/components/shared/user-nav.tsx
@@ -1,15 +1,14 @@
 
 "use client"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
+import { User } from "lucide-react"
 
 export function UserNav() {
   return (
     <Button variant="ghost" className="relative h-10 w-10 rounded-full p-0">
-      <Avatar className="h-10 w-10">
-        <AvatarImage src="https://placehold.co/40x40/EFEFEF/7F7F7F.png?text=P" alt="@user" data-ai-hint="person man" />
-        <AvatarFallback>U</AvatarFallback>
-      </Avatar>
+       <div className="h-10 w-10 rounded-full flex items-center justify-center bg-muted">
+        <User className="h-5 w-5 text-muted-foreground" />
+       </div>
     </Button>
   )
 }
