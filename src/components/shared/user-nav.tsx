c395b9b3eac493821ca7f936c7772f588ceaa207
Shahbaz Khan <carpm.shahbaz@gmail.com>
2025-07-30 19:18:58 +0000

    when profile icon is clicked here then redirect to the login page

--- src/components/shared/user-nav.tsx
+++ src/components/shared/user-nav.tsx
@@ -2,10 +2,17 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { User } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export function UserNav() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/login');
+  };
+
   return (
-    <Button variant="ghost" className="relative h-10 w-10 rounded-full p-0">
+    <Button variant="ghost" className="relative h-10 w-10 rounded-full p-0" onClick={handleClick}>
        <div className="h-10 w-10 rounded-full flex items-center justify-center bg-muted border">
         <User className="h-5 w-5 text-muted-foreground" />
        </div>
