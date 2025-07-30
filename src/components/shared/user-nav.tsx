b71db68629e8d3cc65904badeebc882b7ab5088e
Shahbaz Khan <carpm.shahbaz@gmail.com>
2025-07-30 19:16:29 +0000

    put a grey border circle around it

--- src/components/shared/user-nav.tsx
+++ src/components/shared/user-nav.tsx
@@ -6,7 +6,7 @@ import { User } from "lucide-react"
 export function UserNav() {
   return (
     <Button variant="ghost" className="relative h-10 w-10 rounded-full p-0">
-       <div className="h-10 w-10 rounded-full flex items-center justify-center bg-muted">
+       <div className="h-10 w-10 rounded-full flex items-center justify-center bg-muted border">
         <User className="h-5 w-5 text-muted-foreground" />
        </div>
     </Button>
